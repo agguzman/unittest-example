f75ea854bcc8e6c3c57cea6facb8c817ff90d268
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-02-04 11:14:08 -0500

    added more mocking to file check

--- example.py
+++ example.py
@@ -2,9 +2,10 @@
 
 
 import os
+import os.path
 
 
 def rm(filename):
-    if os.path.exists(filename):
+    if os.path.isfile(filename):
         os.remove(filename)
 
--- example.test.py
+++ example.test.py
@@ -8,8 +8,20 @@ from example import rm
 
 class RmTestCase(unittest.TestCase):
 
+    @mock.patch('example.os.path')
     @mock.patch('example.os')
-    def test_rm(self, mock_os):
+    def test_rm(self, mock_os, mock_path):
+
+        mock_path.isfile.return_value = False
+
+        rm('any file')
+
+        # test that the remove call was not called
+        self.assertFalse(mock_os.remove.called, 'Failed to remove file if not present')
+
+        # make the file exist
+        mock_path.isfile.return_value = True
+
         rm('any file')
 
         # test that rm called os.remove with the right parameters
